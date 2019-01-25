4b44905f2769b17f001ef9e8ac3f1882c3e15f0c
Q Smart <qiufei@me.com>
2019-01-25 14:17:39 +0000

    Finish TWD 7

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 # Create your views here.
 
 def index(request):
@@ -8,7 +9,9 @@ def index(request):
     return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
-    context_dict = {'boldmessage': "This, is, a, bold, message!"}
+    context_dict = {'boldmessage': "This, is, a, bold, message!",
+                    'aboutmessage': "This tutorial has been put together by Q Smart."
+                    }
     return render(request, 'rango/about.html', context = context_dict)
 
 def index(request):
@@ -31,3 +34,35 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
 
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form':form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category=None
+
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
+    context_dict = {'form':form, 'category':category}
+    return render(request, 'rango/add_page.html', context_dict)
